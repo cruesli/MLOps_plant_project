fca3304ac0065082b15a812e28119711e4d992ab
HC <hclt01@icloud.com>
2026-01-13 15:46:53 +0100

    add initial fastapi application and unit tests

---
+++ tests/test_api.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from src.plants.api import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hi"}
